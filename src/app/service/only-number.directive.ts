421ef3b596b849ce6c46138e5e9c84d4cea249d5
savarirajunc <71635099+savarirajunc@users.noreply.github.com>
2023-05-29 21:14:39 +0530

    Last Check

--- src/app/service/only-number.directive.ts
+++ src/app/service/only-number.directive.ts
@@ -12,20 +12,6 @@ export class OnlyNumberDirective {
     @HostListener('keydown', ['$event']) onKeyDown(event:any) {
       let e = <KeyboardEvent> event;
       if (this.OnlyNumber == 'true') {
-
-        // if ([46, 8, 9, 27, 13, 110, 190].indexOf(e.keyCode) !== -1 ||
-        //   // Allow: Ctrl+A
-        //   (e.keyCode == 65 && e.ctrlKey === true) ||
-        //   // Allow: Ctrl+C
-        //   (e.keyCode == 67 && e.ctrlKey === true) ||
-        //   // Allow: Ctrl+X
-        //   (e.keyCode == 88 && e.ctrlKey === true) ||
-        //   // Allow: home, end, left, right
-        //   (e.keyCode >= 35 && e.keyCode <= 39)) {
-        //     // let it happen, don't do anything
-        //     return;
-        //   }
-          // Ensure that it is a number and stop the keypress
           if ((e.shiftKey || (e.keyCode < 35 || e.keyCode > 57)) &&
 
           (e.keyCode != 8) && (e.keyCode != 9) && (e.keyCode != 13) && (e.keyCode != 32) &&
